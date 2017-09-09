a2fbce6c14d1f177f7aae0fe668495d9df7b1e83
tawatr <tawat.r2d2@gmail.com>
2017-09-09 10:14:19 +0700

    test

---
+++ variable_and_constant.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+
+// Declaration 
+// Implicit declaration
+var nameString = "Master X"
+
+nameString="Doraemon"
+
+// Constant
+let surnameString = "ABCD"
+
+//surnameString="EFGH" 
