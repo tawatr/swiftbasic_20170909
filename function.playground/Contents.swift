455653884f38047dbaeb76bb902c3baf80f1707f
tawatr <tawat.r2d2@gmail.com>
2017-09-10 11:19:58 +0700

    class2

--- function.playground/Contents.swift
+++ function.playground/Contents.swift
@@ -10,6 +10,15 @@ class MyClass{
     var ADouble: Double = 3.1416
     var ABool: Bool = true;
     
+    func welcome( ) -> Void {
+        print("welcome in MyClass.")
+    }
+    
+    func cal_triangle_area( base: Double, height: Double ) -> Double {
+        let area = base*height/2;
+        return area;
+    }
+    
 }
 
 // Outside of the class
@@ -21,3 +30,32 @@ print( "Name ==> \(myName)")
 tawatrClass.AInt=200
 var myAInt=tawatrClass.AInt
 print( "Integer ==> \(myAInt)" )
+
+
+// Create function
+func welcome( ) -> Void {
+    print( "This is the function welcome")
+}
+
+// Call function
+welcome();
+
+tawatrClass.welcome()
+
+func addPosition1(  ) -> String{
+    let result = "Mr. " + myName
+    return result
+}
+// Call function
+let myResult=addPosition1()
+print( "myResult ==> \(myResult)")
+
+func addPosition2( strPre: String ) -> String{
+    let result = strPre + myName
+    return result
+}
+let myResult2=addPosition2( strPre: "Ms. ")
+print( "myResult2 ==> \(myResult2)")
+
+let area1=tawatrClass.cal_triangle_area(base: 10.0, height: 25.0)
+
